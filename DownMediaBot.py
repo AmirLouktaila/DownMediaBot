e42ec39cd80e03eeeacb694d449696ae8845cf64
AmirLouktaila <86385687+AmirLouktaila@users.noreply.github.com>
2023-08-07 18:41:46 -0700

    Update DownMediaBot.py

--- DownMediaBot.py
+++ DownMediaBot.py
@@ -1,7 +1,11 @@
+from flask import Flask
 import telebot
-import requests,os
+import requests
+import os
 
-Mytext='''
+app = Flask(__name__)
+
+Mytext = '''
 ✅ Download any videos ✅
 ✅ video facebook
 ✅ video pinterest
@@ -9,13 +13,16 @@ Mytext='''
 ✅ video tiktok
 ✅ video shorts youtube
 '''
-    
-bot = telebot.TeleBot("6548441774:AAHGZXS5jq2hNvFvI97krRHzLY3B190GtZs")
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, Mytext)
+
+bot = telebot.TeleBot("YourTokenBot")
+
+@app.route('/', methods=['POST'])
+def index():
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok', 200
 def downvideo(link):
-    url=link
+    url = link
     local_file_name = "newvideo.mp4"
 
     response = requests.get(url)
@@ -26,20 +33,22 @@ def downvideo(link):
         print("File downloaded successfully.")
     else:
         print("Failed to download the file. Status code:", response.status_code)
-
-
-
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, Mytext)
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+    chat_id = message.chat.id
+
     if "facebook" in message.text:
-        bot.send_message(message.chat.id,"wait for send the video ...")
-        r=requests.get(f'https://anyshare-1.powerbots.repl.co/down?url={message.text}')
-        r_json=r.json()
-        vid=r_json.get("facebook")
+        bot.send_message(chat_id, "wait for send the video ...")
+        r = requests.get(f'https://anyshare-1.powerbots.repl.co/down?url={message.text}')
+        r_json = r.json()
+        vid = r_json.get("facebook")
         downvideo(vid)
-        name_videos='newvideo.mp4'
-        video_=open(name_videos,'rb')
-        bot.send_video(message.chat.id,video_)
+        name_videos = 'newvideo.mp4'
+        video_ = open(name_videos, 'rb')
+        bot.send_video(chat_id, video_)
         os.remove(name_videos)
 
     elif "pin" in message.text:
@@ -52,7 +61,7 @@ def echo_all(message):
         video_=open(name_videos,'rb')
         bot.send_video(message.chat.id,video_)
         os.remove(name_videos)
-    elif "instgram" in message.text:
+    elif "instagram" in message.text:
         bot.send_message(message.chat.id, "wait for send the video ...")
         r=requests.get(f'https://anyshare-1.powerbots.repl.co/down?url={message.text}')
         r_json=r.json()
@@ -61,7 +70,7 @@ def echo_all(message):
         name_videos='newvideo.mp4'
         video_=open(name_videos,'rb')
         bot.send_video(message.chat.id,video_)
-        os.remove(name_videos)²
+        os.remove(name_videos)
     elif "tiktok" in message.text:
         bot.send_message(message.chat.id, "wait for send the video ...")
         r=requests.get(f'https://anyshare-1.powerbots.repl.co/down?url={message.text}')
@@ -76,7 +85,7 @@ def echo_all(message):
         bot.send_message(message.chat.id, "wait for send the video ...")
         r=requests.get(f'https://anyshare-1.powerbots.repl.co/down?url={message.text}')
         r_json=r.json()
-        vid=r_json.get("shorts")[0].get('cover')
+        vid=r_json.get("shorts")[0].get('vid')
         downvideo(vid)
         name_videos='newvideo.mp4'
         video_=open(name_videos,'rb')
@@ -84,4 +93,7 @@ def echo_all(message):
         os.remove(name_videos)
 
 
-bot.infinity_polling()
+bot.polling()
+
+
+app.run(host='0.0.0.0', port=81)
